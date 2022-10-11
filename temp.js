99c3295bfb477219411a2074dc759ef357855dab
张森 <zs01544071@alibaba-inc.com>
2022-10-11 14:59:38 +0800

    线段

--- temp.js
+++ temp.js
@@ -2,31 +2,30 @@ import * as THREE from "./node_modules/three/build/three.module.js";
 
 function run() {
     let renderer = new THREE.WebGLRenderer({
-        antialias: true
+        antials: true
     });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor("#000");
-    document.body.appendChild(renderer.domElement);
+    renderer.setSize(window.innerWidth, 600);
+
+    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / 600, 1, 1000);
+    camera.position.set(0, 0, 50);
+    camera.lookAt(0, 0, 0);
 
     let scene = new THREE.Scene();
 
-    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(0,0,10);
-    camera.lookAt(0,0,0);
-
-    let geo = new THREE.BoxGeometry(1,1,1);
-    let material = new THREE.MeshBasicMaterial(
-        {
-            color: "pink"
-        }
-    );
-
-    let mesh = new THREE.Mesh(geo, material);
-
-    scene.add(mesh);
+    let vertices = [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, 10)
+    ]
+    let geometry = new THREE.BufferGeometry().setFromPoints(vertices);
+    
+    let material = new THREE.LineBasicMaterial({
+        color: "#0000ff"
+    });
+    let line = new THREE.Line(geometry, material);
+    scene.add(line);
 
     renderer.render(scene, camera);
-    
+
 
 }
 
